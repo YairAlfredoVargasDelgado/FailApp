644fccbc9da972b6c4b9d5fd9315a334bb9c5cf8
zipusbraid91 <zipusbraid91@gmail.com>
2021-12-18 10:35:37 -0500

    Login

---
+++ FailApp/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FailApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+
+       
+    }
+}
+
